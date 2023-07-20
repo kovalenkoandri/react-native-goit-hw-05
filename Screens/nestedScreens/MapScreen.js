067a68dd644d82354c7124ec2873dd8b0d1f18f0
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-07-20 15:31:16 +0300

    default map

--- Screens/nestedScreens/MapScreen.js
+++ Screens/nestedScreens/MapScreen.js
@@ -1,17 +1,35 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const MapScreen = () => (
   <View style={styles.container}>
-    <Text>MapScreen</Text>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 50.516339,
+        longitude: 30.602185,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.006,
+      }}
+    >
+      <Marker
+        coordinate={{ latitude: 50.516339, longitude: 30.602185 }}
+        title="travel photo"
+      />
+    </MapView>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
 
